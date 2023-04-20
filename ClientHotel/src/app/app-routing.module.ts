0b8943189ad4b4cb6fe994c02e85b1d196908b40
buithithanhlan <lanbtt20406@st.uel.edu.vn>
2023-04-20 14:09:41 +0700

    upload header, footer

--- ClientHotel/src/app/app-routing.module.ts
+++ ClientHotel/src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderMenuComponent } from './header-menu/header-menu.component';
+import { FooterMenuComponent } from './footer-menu/footer-menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"header-menu",component:HeaderMenuComponent},
+  {path:"footer-menu",component:FooterMenuComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const RoutingComponent=[]
+export const RoutingComponent=[
+  HeaderMenuComponent,
+  FooterMenuComponent
+]
--- ClientHotel/src/app/app.module.ts
+++ ClientHotel/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule, RoutingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { HeaderMenuComponent } from './header-menu/header-menu.component';
+import { FooterMenuComponent } from './footer-menu/footer-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RoutingComponent
+    RoutingComponent,
+    HeaderMenuComponent,
+    FooterMenuComponent
   ],
   imports: [
     BrowserModule,
