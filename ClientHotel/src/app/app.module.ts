e376117a25ee9304524fe3cd049ecfe17428a67c
thenhchuc <118499331+thenhchuc@users.noreply.github.com>
2023-04-20 12:23:15 +0700

    add Signin page

--- ClientHotel/src/app/app.module.ts
+++ ClientHotel/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule, RoutingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RoutingComponent
+    RoutingComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
